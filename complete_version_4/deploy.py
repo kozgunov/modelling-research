8493316e2b2ab721481bfa569dd7fb588f4bfe5d
Nikita Kozgunovn <78970376+kozgunov@users.noreply.github.com>
2024-10-16 14:51:11 +0300

    Update deploy.py

--- complete_version_4/deploy.py
+++ complete_version_4/deploy.py
@@ -11,7 +11,7 @@ model = None
 def load_model(model_path):
     global model
     model = YOLOModel(model_name='yolov8n', num_classes=6)
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path)) # loading of the newest model
     model.eval()
 
 @app.route('/predict', methods=['POST'])
@@ -20,9 +20,9 @@ def predict():
         return "Please upload an image or video file", 400
     file = request.files['file']
     
-    if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+    if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')): # apply only png
         return process_image(file)
-    elif file.filename.lower().endswith(('.mp4', '.avi', '.mov')):
+    elif file.filename.lower().endswith(('.mp4', '.avi', '.mov')): # prod.
         return process_video(file)
     else:
         return "Unsupported file format", 400
@@ -45,7 +45,7 @@ def process_image(file):
     return jsonify(detections)
 
 def process_video(file):
-    temp_path = 'temp_video.mp4'
+    temp_path = 'temp_video.mp4' # -----------change by automatization -----------------------------------------------
     file.save(temp_path)
     
     cap = cv2.VideoCapture(temp_path)
@@ -61,12 +61,12 @@ def process_video(file):
             with torch.no_grad():
                 results = model(img_tensor)
             
-            for *xyxy, conf, cls in results.xyxy[0].cpu().numpy():
+            for *xyxy, conf, cls in results.xyxy[0].cpu().numpy(): 
                 cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0, 255, 0), 2)
-                cv2.putText(frame, f'Class: {int(cls)}, Conf: {conf:.2f}', (int(xyxy[0]), int(xyxy[1])-10),
+                cv2.putText(frame, f'Class: {int(cls)}, Conf: {conf:.2f}', (int(xyxy[0]), int(xyxy[1])-10), # who is who
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             
-            _, buffer = cv2.imencode('.jpg', frame)
+            _, buffer = cv2.imencode('.jpg', frame) # save jpg, because it contains more information
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
